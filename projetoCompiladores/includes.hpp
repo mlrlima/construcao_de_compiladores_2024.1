5b9ab126913e1238a061518f1d7481116e2d4e91
Maria Luiza Lima <132663650+mlrlima@users.noreply.github.com>
2024-05-14 08:36:51 -0300

    Create includes.hpp

---
+++ projetoCompiladores/includes.hpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+#include "../../projetoCompiladores/compiladores2.cpp";
+#include "../../projetoCompiladores/projetocompiladores1.cpp";
